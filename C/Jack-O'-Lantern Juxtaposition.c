a5a6726b97f0ae03437654c7ddbc778a10eb19c9
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2021-07-12 14:01:06 +0800

    Create Jack-O'-Lantern Juxtaposition.c

---
+++ C/Jack-O'-Lantern Juxtaposition.c
@@ -0,0 +1,12 @@
+//https://open.kattis.com/problems/jackolanternjuxtaposition
+#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+
+int main() {
+  int a, b, c, total;
+	scanf("%d %d %d", &a, &b, &c); 
+	total = a * b * c;
+	printf("%d", total);
+  return 0;
+}
