926b5da219fcf18056c3d781c7e3f71a84cd6304
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2021-07-13 10:04:27 +0800

    Create Solving for Carrots.c

---
+++ C/Solving for Carrots.c
@@ -0,0 +1,13 @@
+//https://open.kattis.com/problems/carrots
+#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+
+int main() {
+	int size;
+	scanf("%d", &size);
+	int num;
+	scanf("%d", &num);
+	printf("%d", num);
+  return 0;
+}
